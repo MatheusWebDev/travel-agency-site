11b3b82036d5a96754ba1428eebdec63dc0f62de
Matheus <matheussky.mfj@gmail.com>
2018-04-16 02:49:24 -0300

    add and config some tasks in gulp file

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp  		 = require('gulp'),
+	watch 		 = require('gulp-watch'),
+	cssVars 	 = require('postcss-simple-vars'),
+	postcss 	 = require('gulp-postcss'),
+	cssNested	 = require('postcss-nested'),
+	autoprefixer = require('autoprefixer');
+
+
+gulp.task('default', function(){
+	console.log("Ok - You create a gulp task.");
+});
+
+gulp.task('html', function(){
+	console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/style.css')
+		.pipe(postcss([cssVars, cssNested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+	
+	watch('./app/index.html', function() {
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	});
+
+});
