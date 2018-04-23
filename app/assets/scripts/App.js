287d5fcf0b17695558803b607d7c9ebcabccde74
Matheus <matheussky.mfj@gmail.com>
2018-04-23 02:01:22 -0300

    Completed desktop and mobile header styles.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,2 @@
-var $ = require("jquery");
-//var Person = require("./modules/Person");
-import Person from "./modules/Person";
-
-class Adult extends Person {
-    payTaxes(){
-        console.log(this.name + " now owns $0 in taxes.");
-    }
-}
-    
-alert("ABC 1234");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+import MobileMenu from './modules/MobileMenu';
+var mobileMenu = new MobileMenu();
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+class MobileMenu {
+	constructor() {
+		this.siteHeader = $(".site-header");
+		this.menuIcon = $(".site-header__menu-icon");
+		this.menuContent = $(".site-header__menu-content");
+		this.events();
+	}
+
+	events() {
+		this.menuIcon.click(this.troggleTheMenu.bind(this));
+	}
+
+	troggleTheMenu() {
+		this.menuContent.toggleClass("site-header__menu-content--visible");
+		this.siteHeader.toggleClass("site-header--expanded");
+		this.menuIcon.toggleClass("site-header__menu-icon--close-x");
+	}
+}
+export default MobileMenu;
